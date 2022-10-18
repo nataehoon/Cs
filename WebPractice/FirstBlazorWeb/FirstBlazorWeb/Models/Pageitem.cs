fdad0c9283d532d7f8615010009f3279cfc60b6b
taehoon <skeoddla@gmail.com>
2022-10-18 22:47:38 +0900

    pagination update

---
+++ WebPractice/FirstBlazorWeb/FirstBlazorWeb/Models/Pageitem.cs
@@ -0,0 +1,17 @@
+﻿namespace FirstBlazorWeb.Models
+{
+    public class Pageitem
+    {
+        public string Text { get; set; }
+        public int PageIndex { get; set; }
+        public bool Enabled { get; set; }
+        public bool Active { get; set; }
+
+        public Pageitem(int pageIndex, bool enabled, string text)
+        {
+            Text = text;
+            PageIndex = pageIndex;
+            Enabled = enabled;
+        }
+    }
+}
